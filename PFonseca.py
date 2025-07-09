dd7ebbf12e9e69d61cd73dd73c807d71644691be
paulom40 <66902424+paulom40@users.noreply.github.com>
2025-07-09 13:25:50 +0100

    Update PFonseca.py

--- PFonseca.py
+++ PFonseca.py
@@ -33,6 +33,8 @@ df = pd.read_excel(BytesIO(response.content), sheet_name="PFonseca2")
 
 st.write("📅 Last Update 04/07/2025")
 
+st.write("📅 Datas convertidas corretamente:")
+st.write(df["Data Venc."].head(10))
 
 
 
