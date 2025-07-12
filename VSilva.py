905066eb4fb2b5db3a24918e2e9dfbd9a160b490
paulom40 <66902424+paulom40@users.noreply.github.com>
2025-07-12 07:41:36 +0100

    Update VSilva.py

--- VSilva.py
+++ VSilva.py
@@ -27,7 +27,7 @@ except Exception as e:
     st.error(f"Erro ao carregar os dados: {e}")
     st.stop()
 
-st.write("📅 Last Update 04/07/2025")
+st.write("📅 Last Update 11/07/2025")
 
 # -------------------------------
 # 🧹 Clean and prepare data
