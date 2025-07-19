cfdc521d4ecb82306090a58f89ea9099b69d1a85
paulom40 <66902424+paulom40@users.noreply.github.com>
2025-07-19 08:06:26 +0100

    Update BBrito.py

--- BBrito.py
+++ BBrito.py
@@ -80,8 +80,8 @@ col3.metric("💰 Valor Pendente Total", f"€ {valor_total:,.2f}")
 # -------------------------------
 # 📉 Extra Table: Atrasados - Dias -60 a -1
 # -------------------------------
-st.subheader("📉 Registros Por vencer nos Últimos 60 Dias")
-df_atrasado = df_cliente[(df_cliente["Dias"] >= -60) & (df_cliente["Dias"] <= -1)]
+st.subheader("📉 Registros Por vencer nos Últimos 7 Dias")
+df_atrasado = df_cliente[(df_cliente["Dias"] >= -7) & (df_cliente["Dias"] <= -1)]
 st.dataframe(df_atrasado, use_container_width=True)
 
 # 📈 Metrics for overdue table
