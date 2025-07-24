b882d0ac404a2a3cba76458a515eb59075497d8c
paulom40 <66902424+paulom40@users.noreply.github.com>
2025-07-24 06:33:55 +0100

    Update MMiranda.py

--- MMiranda.py
+++ MMiranda.py
@@ -18,7 +18,7 @@ except Exception as e:
     st.error(f"Erro ao carregar os dados: {e}")
     st.stop()
 
-st.write("📅 Last Update 18/07/2025")
+st.write("📅 Last Update 23/07/2025")
 
 # -------------------------------
 # 🧹 Clean and prepare data
