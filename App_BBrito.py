7473cc8219b2f17777844bbccc2933df9f47ea38
paulom40 <66902424+paulom40@users.noreply.github.com>
2025-07-31 18:32:27 +0100

    Update App_BBrito.py

--- App_BBrito.py
+++ App_BBrito.py
@@ -23,7 +23,7 @@ except Exception as e:
 # ---------------------------------------
 # 📅 Data Cleaning
 # ---------------------------------------
-st.write("📅 Última atualização: 26/07/2025")
+st.write("📅 Última atualização: 31/07/2025")
 
 df.columns = df.columns.str.strip()
 df["Entidade"] = df["Entidade"].astype(str).str.strip()
