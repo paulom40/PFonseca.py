e9e43601bfd971ef1618add4b5e61d2c667ab5bc
paulom40 <66902424+paulom40@users.noreply.github.com>
2025-08-16 12:31:02 +0100

    Update Alertas.py

--- Alertas.py
+++ Alertas.py
@@ -215,4 +215,5 @@ def dashboard_page():
         st.rerun()
 
     st.markdown("---")
-    st.markdown("<p style='text-align: center; color: #ffffff;'>Created with ❤️ using Streamlit</p")
+    st.markdown("<p style='text-align: center; color: #ffffff;'>Created with ❤️ using Streamlit</p>", unsafe_allow_html=True)
+
