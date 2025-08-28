70f93fafcf07158445aa44644199d21ca3a538b4
paulom40 <66902424+paulom40@users.noreply.github.com>
2025-08-28 08:22:25 +0100

    Update Bolama.py

--- Bolama.py
+++ Bolama.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+# 🚀 Page configuration
+st.set_page_config(page_title="Sales Dashboard", layout="wide", page_icon="📊")
+
 st.markdown("""
     <style>
     #MainMenu {visibility: hidden;}
